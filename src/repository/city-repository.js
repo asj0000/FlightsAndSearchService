908d98b92544e0d4fa7de3aeea3e1367f94e112c
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2025-06-25 15:47:47 +0530

    Completed City repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -5,11 +5,12 @@ class CityRepostory {
   // METHOD to add a new city entry in our Table
   async createCity( { name }){
     try{
-      const city = await City.create( { name});
+      const city = await City.create( {name});
       return city;
 
     }catch(error){
-      throw { error };
+      console.log("Error in Creating City ");
+      throw{error};
     }
 
   }
@@ -24,10 +25,39 @@ class CityRepostory {
       });
 
     }catch( error ){
+      console.log("Error in Deleting City -- ");
       throw { error };
     }
 
   }
+
+  async getCity({cityId }){
+    try{
+      const city = await City.findByPk( cityId) ;
+      console.log( "City is - ", city);
+    }catch( error ){
+      console.log("Error in Getting City -- ");
+      throw { error };
+    }
+  }
+
+  async updateCity( cityId , data){
+    try{
+      const cityObj = await City.update( data , {
+        where:{
+          id : cityId
+        }
+      });
+
+      console.log( "Updated Onject  --", cityObj);
+      return cityObj ; 
+    }catch(error){
+      console.log("Error in Updating City -- ");
+      throw { error };
+    }
+  }
+
+   
 }
 
 module.exports = CityRepostory;
