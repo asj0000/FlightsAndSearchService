d504f8a01175e3d6d30aab380a53df0d9420f8d3
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2025-06-26 09:43:50 +0530

    Added city service layer

---
+++ src/repository/index.js
@@ -0,0 +1,5 @@
+// This object which we are exporting will contain all the repositories
+// which we want to export to other files
+module.exports = {
+  CityRepository : require('./city-repository'),
+}
---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+  constructor(){
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data){
+    try {
+      const city = await this.cityRepository.createCity( data );
+      return city;
+    } catch (error) {
+      console.log("Error in Creating City Service Layer -- ", error);
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId ,  data){
+    try{
+      const cityObj = await this.cityRepository.updateCity( cityId , data );
+      return cityObj;
+    }catch(error){
+      console.log("Error in Updating City Service laye -- ", error);
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId){
+    try{
+      await this.cityRepository.deleteCity(cityId);
+    }catch(error){
+      console.log("Error in Deleting City Service Layer -- ", error);
+      throw { error };
+    }
+  }
+
+  async getCity(cityId){
+    try {
+      const city = await this.cityRepository.getCity( cityId );
+      return city;
+    } catch (error) {
+      console.log("Error in getting City Service Layer -- ", error);
+      throw { error };
+    }
+  }
+}
+
+module.exports = CityService;
